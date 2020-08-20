370f8981e114525c8e40677b1e3b946237ce2d53
Nikos Maragkos <soadnick@outlook.com>
2020-08-20 14:03:31 +0300

    Add files via upload

---
+++ src/scripts/login.php
@@ -0,0 +1,19 @@
+<?php
+    $link = mysqli_connect("localhost", "nikosm", "1q2w3e4r", "site");
+
+    $username = $_POST["username"];
+    $hashed_password = md5($_POST["password"]);
+
+    $result = mysqli_query($link, "SELECT * FROM users WHERE name='$username' AND password='$hashed_password'");
+
+    if (mysqli_num_rows($result) == 1) {
+        printf("successful login.\n");
+    }
+    else {
+        printf("wrong username or password.\n");
+    }
+
+    mysqli_free_result($result);
+
+    mysqli_close($link);
+?>
