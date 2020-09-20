fa68e99a46a8b0d926fd7d67f4c20057ad77a08d
nikos <soadnick@outlook.com>
2020-09-20 04:26:10 +0300

    fixed grid in admin dashboard

--- src/adminpanel.php
+++ src/adminpanel.php
@@ -63,10 +63,10 @@
 
         <div class="container-fluid pt-3"  style="background-color: #292b2c">
           <div class="row align-items-start">
-            <div class="col-sm-3">
+            <div class="col-4">
                 <h2 id="GraphLabel"><u>Users Activities Percentage</u></h2>
                 <canvas id="ChartA"></canvas>
-                <div class="col-lg-3">
+                <div class="col">
                     <table  class="table table-striped table-dark">
                         <thead>
                             <tr>
@@ -80,7 +80,7 @@
                     </table>
                 </div>
             </div>
-            <div class="col"> 
+            <div class="col-4"> 
              <h2 id="GraphLabel"><u>Number of users per Registers</u></h2>
               <canvas id="ChartB"></canvas>
               <div class="col">
@@ -97,11 +97,10 @@
                     </table>
                 </div>
             </div>
-            <div class="col">
+            <div class="col-4">
                 <h2 id="GraphLabel"><u>Registers per month</u></h2>
                 <canvas id="ChartC"></canvas>
                 <div class="col">
-                  TABLE C
                   <table  class="table table-striped table-dark">
                         <thead>
                             <tr>
@@ -109,17 +108,14 @@
                                 <th scope="col">Registers</th>
                             </tr>
                         </thead>
-                        <tbody  id="tableC">
-                                    
-                        </tbody>
+                        <tbody  id="tableC"></tbody>
                     </table>
                 </div>
             </div>
-            <div class="col">
-                <h2 id="GraphLabel"><u>Registers per day of the week</u></h2>
+            <div class="col-4">
+                <h2 id="GraphLabel"><u>Registers per weekday</u></h2>
                 <canvas id="ChartD"></canvas>
                 <div class="col">
-                    TABLE D
                     <table  class="table table-striped table-dark">
                         <thead>
                             <tr>
@@ -127,17 +123,14 @@
                                 <th scope="col">Registers</th>
                             </tr>
                         </thead>
-                        <tbody  id="tableD">
-                                        
-                        </tbody>
+                        <tbody  id="tableD"></tbody>
                     </table>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-4">
                 <h2 id="GraphLabel"><u>Registers per hour</u></h2>
                 <canvas id="ChartE"></canvas>
-                <div class="col-sm-3">
-                    TABLE E
+                <div class="col">
                     <table  class="table table-striped table-dark">
                         <thead>
                             <tr>
@@ -145,17 +138,14 @@
                                 <th scope="col">Registers</th>
                             </tr>
                         </thead>
-                        <tbody  id="tableE">
-                                        
-                        </tbody>
+                        <tbody  id="tableE"></tbody>
                     </table>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-4">
               <h2 id="GraphLabel"><u>Registers per year</u></h2>
                 <canvas id="ChartF"></canvas>
-                <div class="col-sm-3">
-                    TABLE F
+                <div class="col">
                     <table  class="table table-striped table-dark">
                         <thead>
                             <tr>
@@ -163,9 +153,7 @@
                                 <th scope="col">Registers</th>
                             </tr>
                         </thead>
-                        <tbody  id="tableF">
-                                        
-                        </tbody>
+                        <tbody  id="tableF"></tbody>
                     </table>
                 </div>
             </div>
